ae57da23c19d84d7373fd0235a907426148ae11d
Cherimolah <inkustregister@mail.ru>
2023-06-19 03:04:40 +0500

    Fix config import

--- telegram_bot/loader.py
+++ telegram_bot/loader.py
@@ -1,6 +1,6 @@
 from aiogram import Bot, Dispatcher
 
-from config import BOT_TOKEN
+from telegram_bot.config import BOT_TOKEN
 from telegram_bot.middlewares import CreateUserMiddleware
 
 bot = Bot(BOT_TOKEN)
